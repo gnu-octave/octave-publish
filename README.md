1ec119774f03bfb1e073c6df8e9f336c99e73271
Kai T. Ohlhus <k.ohlhus@gmail.com>
2016-05-10 14:00:08 +0200

    Start with code from Octave-forge package. Added test script. Minor README update.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # octave-publish
 
-Create a Matlab compatible [publish](https://www.mathworks.com/help/matlab/ref/publish.html)
-and [grabcode](https://www.mathworks.com/help/matlab/ref/grabcode.html) function.
+Create a Matlab compatible
+[publish](https://www.mathworks.com/help/matlab/ref/publish.html)
+and [grabcode](https://www.mathworks.com/help/matlab/ref/grabcode.html)
+function.
 
-The initial version is available from the [Octave-forge package "miscellaneous"]( http://octave.sourceforge.net/miscellaneous/function/publish.html).
+The initial version is available from the
+[Octave-forge package "miscellaneous"]( http://octave.sourceforge.net/miscellaneous/function/publish.html).
---
+++ test_script.m
@@ -0,0 +1,209 @@
+%% Headline
+% Headline description.
+%%
+
+disp ("First recognized Octave code after %%")
+
+%% SECTION TITLE
+% DESCRIPTIVE TEXT
+
+%%% SECTION TITLE WITHOUT SECTION BREAK
+% DESCRIPTIVE TEXT
+
+## SECTION TITLE
+# DESCRIPTIVE TEXT
+
+### SECTION TITLE WITHOUT SECTION BREAK
+# DESCRIPTIVE TEXT
+
+% some real comment
+x = 0:0.01:2*pi
+
+# some real comment
+y = sin(x)
+
+%% Text formatting
+% PLAIN TEXT _ITALIC TEXT_ *BOLD TEXT* |MONOSPACED TEXT|
+% |MONOSPACED TEXT| PLAIN TEXT _ITALIC TEXT_ *BOLD TEXT*
+% *BOLD TEXT* |MONOSPACED TEXT| PLAIN TEXT _ITALIC TEXT_
+% _ITALIC TEXT_ *BOLD TEXT* |MONOSPACED TEXT| PLAIN TEXT
+% Trademarks:
+% TEXT(TM)
+% TEXT(R)
+
+% figure code
+plot (x,y)
+
+## Text formatting
+# PLAIN TEXT _ITALIC TEXT_ *BOLD TEXT* |MONOSPACED TEXT|
+# |MONOSPACED TEXT| PLAIN TEXT _ITALIC TEXT_ *BOLD TEXT*
+# *BOLD TEXT* |MONOSPACED TEXT| PLAIN TEXT _ITALIC TEXT_
+# _ITALIC TEXT_ *BOLD TEXT* |MONOSPACED TEXT| PLAIN TEXT
+# Trademarks:
+# TEXT(TM)
+# TEXT(R)
+
+%% Bulleted List
+%
+% * BULLETED ITEM 1
+% * BULLETED ITEM 2
+%
+
+## Bulleted List
+#
+# * BULLETED ITEM 1
+# * BULLETED ITEM 2
+#
+
+%% Numbered List
+%
+% # NUMBERED ITEM 1
+% # NUMBERED ITEM 2
+%
+
+## Numbered List
+#
+# # NUMBERED ITEM 1
+# # NUMBERED ITEM 2
+#
+
+%%
+%
+%  PREFORMATTED
+%  TEXT
+%
+
+##
+#
+#  PREFORMATTED
+#  TEXT
+#
+
+%% GNU Octave Code
+%
+%   for i = 1:10
+%     disp (x)
+%   endfor
+%
+
+## GNU Octave Code
+#
+#   for i = 1:10
+#     disp (x)
+#   endfor
+#
+
+%% External File Content
+% 
+% <include>fname.m</include>
+%
+
+## External File Content
+#
+# <include>fname.m</include>
+#
+
+%% External Graphic
+%
+% <<myGraphic.png>>
+%
+
+## External Graphic
+#
+# <<myGraphic.png>>
+#
+
+%% Inline LaTeX
+% $f(n) = n^5 + 4n^2 + 2 |_{n=17}$
+
+## Inline LaTeX
+# $f(n) = n^5 + 4n^2 + 2 |_{n=17}$
+
+%% Block LaTeX
+% $$f(n) = n^5 + 4n^2 + 2 |_{n=17}$$
+
+## Block LaTeX
+# $$f(n) = n^5 + 4n^2 + 2 |_{n=17}$$
+
+%% Links
+% <https://www.gnu.org/software/octave>
+% <octave:FUNCTION DISPLAYED_TEXT>
+%
+
+## Links
+# <https://www.gnu.org/software/octave>
+# <octave:FUNCTION DISPLAYED_TEXT>
+#
+
+%% HTML Markup
+% <html>
+% <table border=1><tr>
+% <td>one</td>
+% <td>two</td></tr></table>
+% </html>
+%
+
+## HTML Markup
+# <html>
+# <table border=1>
+# <tr>
+# <td>one</td>
+# <td>two</td>
+# </tr>
+# </table>
+# </html>
+#
+
+%% LaTeX Markup
+% <latex>
+% \begin{pmatrix}
+% 1 & 2 \\ 3 & 4
+% \end{pmatrix}
+% </latex>
+%
+
+## LaTeX Markup
+# <latex>
+# \begin{pmatrix}
+# 1 & 2 \\ 3 & 4
+# \end{pmatrix}
+# </latex>
+#
+
+%% Long void
+%
+%
+%
+%
+%
+%
+%
+%
+% content
+%
+%
+%
+%
+%
+%
+%
+
+## Long void
+#
+#
+#
+#
+#
+#
+#
+# content
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
